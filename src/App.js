78304566139f7dce9cb131f1626d909d224db4da
cindy <cnmnzhang@github.com>
2023-06-08 17:01:05 -0400

    added some introduction text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css"
 import "./styles/Global.css"
 import Intro from './components/Intro'
+import About from './components/About'
 
 const App = () => {
   return (
     <div className='App'>App
       <div id="content">
         <Intro></Intro>
+        <About></About>
 
       </div>
     </div>
